ca554cae6c17d09bd39be08af66f9839b5ddfe89
khanhanhtr <khanhanhtr00@gmail.com>
2023-04-10 16:50:50 +1000

    "Add redux, tailwindcss"

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import { createRoot } from 'react-dom/client';
+import './style/index.css';
+
 import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 
-import App from './App';
-import './index.css';
+import App from './pages/App';
 
-const container = document.getElementById('root');
+const container = document.getElementById('root') as HTMLElement;
 const root = createRoot(container);
 
 root.render(
